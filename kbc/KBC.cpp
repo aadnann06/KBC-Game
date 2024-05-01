32e8c037bb9a731c2489aaf26ebeb1be6ce2a895
Adnan Qureshi <148219310+aadnann06@users.noreply.github.com>
2024-05-01 17:39:24 +0530

    Add files via upload

---
+++ kbc/KBC.cpp
@@ -0,0 +1,392 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d,e,q,yes,user;
+	printf("DIRECTOR__ ANKUSH SIR\n");
+
+	printf("WELCOME TO KBC GAME\n");
+	printf("choose the options\n");
+	
+	printf("Your 1ST question for 1,000 is :\n\n");
+	printf("1. How many days do we have in a week?\n");
+
+	printf("1).7\n");
+	printf("2).9\n");
+	printf("3).2\n");
+	printf("4).5\n");
+	printf("5).QUIT\n");
+	scanf("%d",&q);
+	a=1;
+	b=5;
+	if(q==a)
+	{
+		printf("you win 1,000\n");
+		printf("Your 2ND question for 2,000 is :\n\n\n");
+	printf("2. How many colors are there in a rainbow?\n\n");
+
+	printf("1).2\n");
+	printf("2).6\n");
+	printf("3).7\n");
+	printf("4).8\n");
+	printf("5).QUIT\n");
+	scanf("%d",&q);
+	a=3;
+	b=5;
+	if(q==a)
+	{
+		printf("you win 2,000\n");
+		printf("Your 3RD question for 3,000 is :\n\n\n");
+	    printf("3. Which animal is known as the ‘Ship of the Desert?’\n\n");
+	    
+	printf("1).Horse\n");
+	printf("2).Cat\n");
+	printf("3).Dog\n");
+	printf("4).Camel\n");
+	printf("5).QUIT\n");
+	scanf("%d",&q);
+	a=4;
+	b=5;
+	if(q==a)
+	{
+		printf("you win 3,000\n");
+		printf("Your 4TH question for 5,000 is :\n\n\n");
+	    printf("4. How many sides are there in a triangle?\n\n");
+		
+	
+	printf("1).Two\n");
+	printf("2).One\n");
+	printf("3).Three\n");
+	printf("4).Zero\n");
+	printf("5).QUIT\n");
+	scanf("%d",&q);
+	a=3;
+	b=5;
+	if(q==a)
+	{
+	printf("you win 5,000\n");
+		printf("Your  5TH question for 10,000 is :\n\n\n");
+	    printf("5. Which animal is called King of Jungle?\n\n");
+		
+	
+	printf("1).Tiger\n");
+	printf("2).Lion\n");
+	printf("3).Zebra\n");
+	printf("4).Fox\n");
+	printf("5).QUIT\n");
+	scanf("%d",&q);
+	a=2;
+	b=5;
+	if(q==a)
+	{
+		printf("you win 10,000\n");
+		printf("Your  6TH question for 20,000 is :\n\n\n");
+	    printf("6. Which is the largest animal in the world?\n\n");
+		
+	
+	printf("1).Blue Whale\n");
+	printf("2).Eliphant\n");
+	printf("3).Shark\n");
+	printf("4).Giraf\n");
+	printf("5).QUIT\n");
+	scanf("%d",&q);
+	a=1;
+	b=5;
+	if(q==a)
+	{
+		printf("you win 20,000\n");
+		printf("Your  7TH question for 40,000 is :\n\n\n");
+	    printf("7. Which festival is known as the festival of colors?\n\n");
+		
+	
+	printf("1).Diwali\n");
+	printf("2).Bhai Dooj\n");
+	printf("3).Holi\n");
+	printf("4).Dussehra\n");
+	printf("5).QUIT\n");
+	scanf("%d",&q);
+	a=3;
+	b=5;
+	if(q==a)
+	{
+		printf("you win 40,000\n");
+		printf("Your  8TH question for 80,000 is :\n\n\n");
+	    printf("8. In which direction does the sunrise?\n\n");
+		
+	
+	printf("1).West\n");
+	printf("2).South\n");
+	printf("3).North\n");
+	printf("4).East\n");
+	printf("5).QUIT\n");
+	scanf("%d",&q);
+	a=4;
+	b=5;
+	if(q==a)
+	{
+		printf("you win 80,000\n");
+		printf("Your  9TH question for 1,60,000 is :\n\n\n");
+	    printf("9. Which is the world’s largest flower?\n\n");
+		
+	
+	printf("1).Rafflesia\n");
+	printf("2).Rose\n");
+	printf("3).Sunflower\n");
+	printf("4).Lotus\n");
+	printf("5).QUIT\n");
+	scanf("%d",&q);
+	a=1;
+	b=5;
+	if(q==a)
+	{
+		printf("you win 1,60,000\n");
+		printf("Your  10TH question for 3,20,000 is :\n\n\n");
+	    printf("10. What goes up but never ever comes down?\n\n");
+		
+	
+	printf("1).Coin\n");
+	printf("2).Your AGE\n");
+	printf("3).Ball\n");
+	printf("4).Plane\n");
+	printf("5).QUIT\n");
+	scanf("%d",&q);
+	a=2;
+	b=5;
+	if(q==a)
+	{
+		printf("you win 3,20,000\n");
+		printf("Your  11TH question for 6,40,000 is :\n\n\n");
+	    printf("11. How many seconds are there in a minute?\n\n");
+		
+	
+	printf("1).20\n");
+	printf("2).80\n");
+	printf("3).60\n");
+	printf("4).10\n");
+	printf("5).QUIT\n");
+	scanf("%d",&q);
+	a=3;
+	b=5;
+	if(q==a)
+	{
+		printf("you win 6,40,000\n");
+		printf("Your  12TH question for 12,50,000 is :\n\n\n");
+	    printf("12. Name the National bird of India?\n\n");
+		
+	
+	printf("1).Sparro\n");
+	printf("2).Pegon\n");
+	printf("3).Swan\n");
+	printf("4).Peacock\n");
+	printf("5).QUIT\n");
+	scanf("%d",&q);
+	a=4;
+	b=5;
+	if(q==a)
+	{
+		printf("you win 12,50,000\n");
+		printf("Your  13TH question for 25,00,000 is :\n\n\n");
+	    printf("13. Name the National game of India?\n\n");
+		
+	
+	printf("1).Hockey\n");
+	printf("2).Cricket\n");
+	printf("3).Football\n");
+	printf("4).Kabaddi\n");
+	printf("5).QUIT\n");
+	scanf("%d",&q);
+	a=1;
+	b=5;
+	if(q==a)
+	{
+		printf("you win 25,00,000\n");
+		printf("Your  14TH question for 50,00,000 is :\n\n\n");
+	    printf("14. How many days a February month have in the leap year?\n\n");
+		
+	
+	printf("1).20\n");
+	printf("2).30\n");
+	printf("3).29\n");
+	printf("4).31\n");
+	printf("5).QUIT\n");
+	scanf("%d",&q);
+	a=3;
+	b=5;
+	if(q==a)
+	{
+		printf("you win 50,00,000\n");
+		printf("Your  15TH question for 1 CRORE is :\n\n\n");
+	    printf("15. Who gave the universal law of gravitation?\n\n");
+		
+	
+	printf("1).Robert Boyle.\n");
+	printf("2).Issac Newton\n");
+	printf("3).Zhang Heng.\n");
+	printf("4).Nicolaus Copernicus.\n");
+	printf("5).QUIT\n");
+	scanf("%d",&q);
+	a=2;
+	b=5;
+	if(q==a)
+	{
+		printf("______!! CONGRATULATIONS YOU WON 1,00,00,000 !!_______");
+	}
+	else if(q==b)
+	{
+		printf("you quit the game \n");
+		printf("you won 50,00,000");
+	}
+	else
+	{
+		printf("you win 3,20,000");
+	}	
+	}
+	else if(q==b)
+	{
+		printf("you quit the game \n");
+		printf("you won 25,00,000");
+	}
+	else
+	{
+		printf("you win 3,20,000");
+	}	
+	
+	}
+	else if(q==b)
+	{
+		printf("you quit the game \n");
+		printf("you won 12,50,000");
+	}
+	else
+	{
+		printf("you win 3,20,000");
+	}	
+	}
+	else if(q==b)
+	{
+		printf("you quit the game \n");
+		printf("you won 6,40,000");
+	}
+	else
+	{
+		printf("you win 3,20,000");
+	}	
+	}
+	else if(q==b)
+	{
+		printf("you quit the game \n");
+		printf("you won 3,20,000");
+	}
+	else
+	{
+		printf("you win 3,20,000");
+	}	
+	}
+	else if(q==b)
+	{
+		printf("you quit the game \n");
+		printf("you won 1,60,000");
+	}
+	else
+	{
+		printf("you win 10,000");
+	}	
+	
+	}
+	else if(q==b)
+	{
+		printf("you quit the game \n");
+		printf("you won 80,000");
+	}
+	else
+	{
+		printf("you win 10,000");
+	}	
+	}
+	else if(q==b)
+	{
+		printf("you quit the game \n");
+		printf("you won 40,000");
+	}
+	else
+	{
+		printf("you win 10,000");
+	}	
+	}
+	else if(q==b)
+	{
+		printf("you quit the game \n");
+		printf("you won 20,000");
+	}
+	else
+	{
+		printf("you win 10,000");
+	}	
+	}
+	else if(q==b)
+	{
+		printf("you quit the game \n");
+		printf("you won 10,000");
+	}
+	else
+	{
+		printf("you win 10,000");
+	}	
+	}
+	else if(q==b)
+	{
+		printf("you quit the game \n");
+		printf("you won 5,000");
+	}
+	else
+	{
+		printf("you win 0");
+	}	
+			
+	}
+	else if(q==b)
+	{
+		printf("you quit the game \n");
+		printf("you won 3,000");
+	}
+	else
+	{
+		printf("you win 0");
+	}	
+	}
+	else if(q==b)
+	{
+		printf("you quit the game \n");
+		printf("you won 2,000");
+	}
+	else
+	{
+		printf("you win 0");
+	}
+	}
+	else if(q==b)
+	{
+		printf("you quit the game \n");
+		printf("you won 0");
+	}
+	else
+	{
+		printf("you win 0");
+	}
+	}
+	else if(q==b)
+	{
+		printf("you quit the game \n");
+		printf("you won 0");
+	}
+	else
+	{
+		printf("you lose the game");
+	}
+
+
+	
+	return 0;
+}
+
+
+
